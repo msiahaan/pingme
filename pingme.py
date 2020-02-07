0acfc8740252994a9784e11f6e7656db3c30b5fe
msiahaan <mico.siahaan@gmail.com>
2020-02-08 07:45:20 +0800

    packed button in one row, ping_start will read address from entry widget

--- pingme.py
+++ pingme.py
@@ -19,27 +19,29 @@ class PingMe(object):
 
 class MainWindow(object):
     def __init__(self):
-        self.address_to = "yahoo.com"
-        self.setup_ui()
+    	self.address_to = "8.8.8.8" #default address to ping
+    	self.setup_ui()
 
     def setup_ui(self):
-        self.window = tk.Tk()
-        self.window.columnconfigure(0, minsize=50)
-        self.window.rowconfigure([0,1,2], minsize=50)
-        self.ent_address = tk.Entry()
-        self.ent_address.insert(0, self.address_to)
-        self.btn_pingme = tk.Button(text="Ping Me", command=self.ping_start)
-        self.btn_stop = tk.Button(text="Stop", command=self.ping_stop)
-        self.ent_address.grid(row=0, column=0)
-        self.btn_pingme.grid(row=1, column=0)
-        self.btn_stop.grid(row=2, column=0)
+      self.window = tk.Tk()
+      self.ent_address = tk.Entry()
+      self.ent_address.insert(0, self.address_to)
+      self.btn_pingme = tk.Button(text="Ping Me", command=self.ping_start)
+      self.btn_stop = tk.Button(text="Stop", command=self.ping_stop)
+      self.ent_address.grid(row=0, column=0, columnspan=2, padx=4)
+      self.btn_pingme.grid(row=1, column=0)
+      self.btn_stop.grid(row=1, column=1)
 
     def ping_start(self):
-        self.ping = PingMe(self.address_to)
-        self.ping_proc = self.ping.ping()
+    	self.address_to = self.ent_address.get()
+    	self.ping = PingMe(self.address_to)
+    	self.ping_proc = self.ping.ping()
 
     def ping_stop(self):
-        self.ping_proc.terminate()
+    	try:
+    		self.ping_proc.terminate()
+    	except:
+    		pass
 
 
 if __name__ == '__main__':
